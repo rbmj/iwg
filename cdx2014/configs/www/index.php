347b6127bbed7b981cb03aa0016e985cc49ec52e
m154500 <m154500@usna.edu>
2014-05-08 11:03:22 -0400

    apache2 example config

---
+++ cdx2014/configs/www/index.php
@@ -0,0 +1,97 @@
+<html>
+<head>
+<title> USNA CDX </title>
+</head>
+<body><h1 style="color:blue"><blink>NAVY CDX</blink></h1>
+<p></p>
+<p>If you're looking for the forum, it's <a href="/forum">here</a></p>
+<br /><p>Taunts and Fun: <a href="taunts.php">here</a></p>
+<h2> New Mail Message Sent to Army</h2>
+<h3> From the USMA Superintedent</h3>
+<p>Public Serivce Announcement</p>
+<p> Greetings,</p> <br />
+<p> In recognition of the humiliating defeats (12 or so, I've stopped keeping track) that USNA has dealt us in the past, I have some guidance to pass down.</p>
+
+<p>First: When we go to IOCTF next year, we need to figure it out. Navy's team destroyed us and it's time for that to change. From what I understand, Navy is embarrassed to even be associated with us.</p>
+<p> Second: Let's field a real team next year. </p>
+<p> Thrid: We need to have better accountability of our mail server convigurations (see:/etc/postfix/main.cf), and actually configure things well.</p>
+<p> Fourth: When Navy sings second next year (for the 13th? time), we will be respectful and professional.</p>
+<p> Fifth: We need to be better at cyber. </p>
+<p> Finally, I award you all sleep ins until graduation. Morale has to improve somehow. </p>
+<br />
+<p> Cheers.</p>
+<p> <b> Go Navy, Beat Army </b></p>
+<p>[#USNA]</p>
+<br /> <br />
+
+<table style="background-color:lightblue;">
+<th>Name</th>
+<th>Role</th>
+</tr>
+<tr>
+<td>Harihara</td>
+<td>CDX Commander/ Network Engineer</td>
+</tr>
+<tr>
+<td>Pfau</td>
+<td>CDX XO/FTP & Proxy Leader</td>
+</tr>
+<tr>
+<td>Arino</td>
+<td>MIA</td>
+</tr>
+<tr>
+<td>Markel</td>
+<td>DNS/Web Leader</td>
+</tr>
+<tr>
+<td>Blair</td>
+<td>Email Leader</td>
+</tr>
+<tr>
+<td>Perkowski</td>
+<td>Active Directory Leader</td>
+</tr>
+<tr>
+<td>Greer</td>
+<td>Snort Leader</td>
+</tr>
+</table>
+
+<p></p>
+
+<table class="tftable" border="1">
+<tr><th>MONDAY</th><th>TUESDAY</th><th>WEDNESDAY</th><th>THURSDAY</th><th>FRIDAY</th></tr>
+<tr><td>0800-1200</td><td>0800-1200</td><td>0800-1200</td><td>0800-1200</td><td>0800-1200</td></tr>
+<tr><td>Markel</td><td>Markel</td><td>Markel</td><td>Markel</td><td>E. Young</td></tr>
+<tr><td>1200-1600</td><td>1200-1600</td><td>1200-1600</td><td>1200-1600</td><td>1200-1600</td></tr>
+<tr><td>Harihara</td><td>Pfau</td><td>Mason</td><td>Perkowski</td><td>E. Young</td></tr>
+<tr><td>1600-2000</td><td>1600-2000</td><td>1600-2000</td><td>1600-2000</td><td>1600-2000</td></tr>
+<tr><td>Pfau</td><td>Greer</td><td>Arino</td><td>Figlioli</td><td>E. Young</td></tr>
+</table>
+
+<p></p>
+
+<table class="tftable" border="1">
+<tr><th>NAME</th><th>RANK</th><th>POSITION</th></tr>
+<tr><td>Harihara</td><td>1/C MIDN</td><td>CDX Commander/ Cisco Wizard</td></tr>
+<tr><td>Pfau</td><td>1/C MIDN</td><td>CDX XO/FTP & Proxy Leader</td></tr>
+<tr><td>Arino</td><td>1/C MIDN</td><td>Fornesics Leader</td></tr>
+<tr><td>Markel</td><td>2/C MIDN</td><td>DNS/Web Leader</td></tr>
+<tr><td>Blair</td><td>3/C MIDN</td><td>Email Leader</td></tr>
+<tr><td>Perkowski</td><td>1/C MIDN</td><td>Active Directory Leader</td></tr>
+<tr><td>Geer</td><td>1/C MIDN</td><td>Snort Leader</td></tr>
+<tr><td>Budzitowski</td><td>2/C MIDN</td><td>Inmate</td></tr>
+<tr><td>Young</td><td>2/C MIDN</td><td>"I have to have a job?"</td></tr>
+<tr><td>Figlioli</td><td>4/C MIDN</td><td>Plebe (Coffee Runner)</td></tr>
+<tr><td>Huang</td><td>3/C MIDN</td><td>Team Member</td></tr>
+<tr><td>Devey</td><td>4/C MIDN</td><td>Team Member</td></tr>
+
+</table>
+<p></p>
+<h3>VoIP Telephone Numbers: </h3>
+<p>Gray Cell: x2071</p>
+<p>White Cell( and all other inquiries): x2072 </p>
+<a href="certsPage.php">Download our web certs</a>
+
+</body></html>
