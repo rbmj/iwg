cf9f2f63f4806547cc97a861ecd2f28a7f589dfd
rbmj <rbmj@verizon.net>
2016-05-18 22:24:55 -0400

    Fix config generator

--- config_switch.sh
+++ config_switch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# USAGE NOTES
+# USAGE NOTES:
+#
 # You'll want to copy and paste this output into a serial
 # terminal after issuing the `conf t` command
 # 
 # If you want to blank the configuration prior to reconf,
 # you can use the write erase command and reboot
+#
+# FINALLY, make sure you issue the `wr mem` command after
+# copying/pasting the outputted configuration
 
 usage () {
     echo "USAGE: $1 [ CENTRAL | RACK ]" 1>&2
@@ -13,19 +17,24 @@ usage () {
     exit 1
 }
 
-output_trunk () {
-    echo interface faste$1
+create_vlan () {
+    echo vlan $1
+    echo name $2
     echo no shutdown
+}
+
+output_trunk () {
+    echo interface gigabite0/$1
+    echo no shutdown
+    echo switchport trunk encap dot1q
     echo switchport mode trunk
-    echo end
 }
 
 output_access () {
-    echo interface faste$1
+    echo interface gigabite0/$1
     echo no shutdown
     echo switchport mode access
     echo switchport access vlan $2
-    echo end
 }
 
 if [ $# != 1 ] ; then
@@ -39,27 +48,31 @@ OUTSIDE_VLAN=201
 WARROOM_VLAN=202
 SERVER_VLAN=203
 
-for i in `seq 0 2 11` ; do
+create_vlan $OUTSIDE_VLAN OUTSIDE
+create_vlan $WARROOM_VLAN WARROOM
+create_vlan $SERVER_VLAN SERVER
+
+for i in `seq 1 2 12` ; do
     # Top row, far left group
     output_trunk $i
 done
 
-for i in `seq 1 2 11` ; do
+for i in `seq 2 2 12` ; do
     # Bottom row, far left group
     output_access $i $OUTSIDE_VLAN
 done
 
-for i in `seq 12 2 23` ; do
+for i in `seq 13 2 24` ; do
     # Top row, center left group
     output_access $i $WARROOM_VLAN
 done
 
-for i in `seq 13 2 23` ; do
+for i in `seq 14 2 24` ; do
     # Bottom row, center left group
     output_access $i $SERVER_VLAN
 done
 
-for i in `seq 24 35` ; do
+for i in `seq 25 36` ; do
     # Center right group
     if [ "x$1" != xCENTRAL ] ; then
         # RACK configuration
@@ -70,7 +83,7 @@ for i in `seq 24 35` ; do
     fi
 done
 
-# Ports 36-47 are reserved for CDX
+# Ports 37-48 are reserved for CDX
 if [ "x$1" != xCENTRAL ] ; then
     # Put CDX configuration for RACK switch here
     echo # Blank line to keep bash from choking
@@ -78,3 +91,5 @@ else
     # Put CDX configuration for CENTRAL switch here
     echo # Blank line to keep bash from choking
 fi
+
+echo end
